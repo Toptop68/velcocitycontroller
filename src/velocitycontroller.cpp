1ab7a31c400210f02dea9cd409f081d925a46499
Toptop68 <krisdaug2@gmail.com>
2019-11-18 09:56:02 +0100

    Initial commit

---
+++ src/velocitycontroller.cpp
@@ -0,0 +1,71 @@
+#include "ros.h"
+#include <std_msgs/Header>
+#include <std_msgs/Empty.h>
+#include <yocs_controllers/velocitycontrol.hpp>
+#include <yocs_controllers/default_controller.hpp>
+#include <string>
+#include <people_msgs/PositionMeasurement.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+ros::init(argc, argv, "velocitycontrol");
+
+ros::NodeHandle n;
+
+float DistanceToFace = 1; //Data from sensors
+ros::Rate loop_rate(2);
+
+while (ros::ok())
+  {
+ros::Subscriber sub = n.subscribe("/sensorPosition", 1000, SetParameters);
+ros::spinOnce();
+loop_rate.sleep();
+  }
+}
+
+
+void SetParameters(){
+
+  n.setParam("max_vel_x", to_string(-(1.8/0.7)*(DistanceToFace)+3.78));
+/*
+  if(DistanceToFace > 3) { // Checks DistanceToFace float and publishes fitting velocity parameters.
+    n.setParam("max_vel_x", "0.0"); // 0% of max speed
+    n.setParam("min_vel_X", "0.0");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+
+  else if (DistanceToFace > 2.64 && DistanceToFace < 3){ //
+    n.setParam("max_vel_x", "0.14"); // 20% of max speed
+    n.setParam("min_vel_X", "0.0");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+  else if(DistanceToFace > 2.28 && DistanceToFAce < 2.65){
+    n.setParam("max_vel_x", "0.28"); // 40% of max speed
+    n.setParam("min_vel_X", "0.0");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+  else if(DistanceToFace > 1.92 && DistanceToFace < 2.29){
+    n.setParam("max_vel_x", "0.42"); // 60% of max speed
+    n.setParam("min_vel_X", "0.0");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+  else if(DistanceToFace > 1.56 && DistanceToFace < 1.93){
+    n.setParam("max_vel_x", "0.56"); // 80% of max speed
+    n.setParam("min_vel_X", "0.0");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+  else if(DistanceToFace > 1.2 && DistanceToFace < 1.57){
+    n.setParam("max_vel_x", "0.7"); // 100% of max speed
+    n.setParam("min_vel_X", "0.2");
+    n.setParam("max_rotational_vel", "3.14");
+    n.setParam("min_in_place_rotational_vel", "3.14");
+  }
+  */
+}
